6570b7a651171a11b0d84676987ebcd3472365df
Ryan Raposo <raposo.ryan@gmail.com>
2020-03-12 13:56:24 -0400

    RELEASE: dist included

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,1 +1,1 @@
-pyinstaller -w --add-data="./img/dstctl.ico;./img" --icon="./img/dstctl.ico" --add-data="./data/ini/cluster_configuration.json;./data/ini" --add-data="./data/ini/shard_configuration.json;./data/ini" dstctl.py
+pyinstaller -n="Shard Projector" -w --noconfirm --icon="./img/dstctl.ico" --add-data="./img/*;./img" --add-data="./data/ini/cluster_configuration.json;./data/ini" --add-data="./data/ini/shard_configuration.json;./data/ini" dstctl.py
