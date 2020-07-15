b00e1dfbb340e39364c7d524c686c9d17849576d
Ryan Raposo <raposo.ryan@gmail.com>
2020-07-15 15:33:32 -0400

    format (black)

--- source/util.py
+++ source/util.py
@@ -3,6 +3,7 @@ from subprocess import Popen, PIPE
 from threading import Thread
 from queue import Queue, Empty
 
+
 def iter_except(function, exception):
     """Iter-like that stops on exception."""
     try:
@@ -16,6 +17,7 @@ class Job:
     """A system job with threaded output queueing. Has methods for controlling
     and monitoring a supplied subprocess.
     """
+
     def __init__(self, args):
         self.process = Popen(args, stdout=PIPE, stdin=PIPE, shell=True)
         self.q = Queue(maxsize=1024)
@@ -41,15 +43,14 @@ class Job:
                 return None
             else:
                 return line
-    
+
     def write_input(self, line):
         """Writes a line to the stdin of the Job's associated process."""
         if self.process:
             with self.process.stdin as pipe:
                 pipe.write(line.encode())
-            
+
     def terminate(self):
         """Kills the Job's process and ceases all threaded activity."""
         self.process.kill()
         self.thread_reader = None
-
