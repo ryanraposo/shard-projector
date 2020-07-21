93e48f2f4207cc114d02af53cc5a268674501f11
Ryan Raposo <raposo.ryan@gmail.com>
2020-07-20 22:11:37 -0400

    Cleanup commented code in constants.py

--- source/constants.py
+++ source/constants.py
@@ -8,26 +8,6 @@ DIR_INI = os.path.join(DIR_ROOT, "ini")
 DIR_TEMP = os.path.join(DIR_ROOT, "temp")
 DIR_EXT = os.path.join(DIR_ROOT, "ext")
 DIR_IMG = os.path.join(DIR_ROOT, "img")
-    
-# STEAMCMD = {
-#     "REQUIRES" : None, # Check for...
-#     "DOWNLOAD" : "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip", # Download from...
-#     "UNPACK" : "steamcmd", # Unzip to...
-#     "INSTALL" : "steamcmd/steamcmd.exe", # Install with...
-#     "PATH" : "steamcmd", # Find at...
-#     "EXECUTE" : "steamcmd/steamcmd.exe", # Run with...
-#     "UPDATE" : None # Update with...
-# }
-# NULLRENDERER = {
-#     "REQUIRES" : STEAMCMD,
-#     "DOWNLOAD" : None,
-#     "UNPACK" : None,
-#     "INSTALL" : "steamcmd/steamcmd.exe +login anonymous +app_update 343050 validate +quit",
-#     "PATH" : "steamcmd/steamapps/common/Don't Starve Together Dedicated Server",
-#     "EXECUTE" : "steamcmd/steamapps/common/Don't Starve Together Dedicated Server/bin/dontstarve_dedicated_server_nullrenderer.exe",
-#     "UPDATE" : "steamcmd/steamcmd.exe +login anonymous +app_update 343050 validate +quit"
-# }
-
 
 if __name__ == "__main__":
     for each in [DIR_ROOT, DIR_INI, DIR_IMG, DIR_EXT]:
